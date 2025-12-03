3c085a59fd38e45b57e88cdedfe6cad889a8afd2
TranHuy <emhuy7a@gmail.com>
2025-12-04 02:13:42 +0700

    define

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,21 @@
+"use client";
 
-import Image from "next/image";
-import { redirect } from "next/navigation";
-import GameHome from "../View/GameHome";
-import { io } from "socket.io-client";
-import GameClient from "../View/GameClient";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
- 
-  return ( 
-        <GameClient />
-    );
-}
+const SKINS = [
+  { id: "red", name: "Đỏ", img: "/skins/red.png" },
+  { id: "mint", name: "Đỏ", img: "/skins/mint.png" },
+  { id: "ocean", name: "Đại Dương", img: "/skins/ocean.png" },
+  { id: "lemon", name: "Chanh Tươi", img: "/skins/lemon.png" },
+  { id: "dark", name: "Bóng Đêm", img: "/skins/dark.png" },
+];
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [activeMenu, setActiveMenu] = useState<"settings" | "keyboard" | null>(
+    null
+  );
+  const [volume, setVolume] = useState(50);
+  const [skinIndex, setSkinIndex] = useState(0);
+  const router = useRouter(); 
